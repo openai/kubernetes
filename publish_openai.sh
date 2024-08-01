a84a543e4d2db8c3feadc68ee7442e5001a50a56
alexzielenski <zielenski@openai.com>
2024-07-31 17:28:51 -0700

    fixup script

--- publish_openai.sh
+++ publish_openai.sh
@@ -15,6 +15,8 @@ fi
 TAG="v1.$X.$Y-openai$Z"
 ACTUAL_TAG=$(git describe --tags --exact-match)
 
+# Tag should match git tag so the version injected into the binaries by k8s
+# build process matches the name of the uploaded binaries
 if [ "$TAG" != "$ACTUAL_TAG" ]; then
     echo "Tag mismatch: $TAG != $ACTUAL_TAG"
     echo "Ensure your commit is tagged with the version you are trying to upload"
@@ -39,16 +41,27 @@ PLATFORMS=(
 # proper deps
 # You may need to edit "build/build-image/Dockerfile" to include:
 # RUN apt update && apt install -y gcc-x86-64-linux-gnu gcc-arm-linux-gnueabihf
-make release KUBE_BUILD_PLATFORMS="${PLATFORMS[@]}" TARGETS="${TARGETS[@]/#/cmd/}" KUBE_RELEASE_RUN_TESTS=n
+export KUBE_BUILD_PLATFORMS="${PLATFORMS[@]}"
+export KUBE_BUILD_CONFORMANCE=n
 
-for TARGET in "${TARGETS[@]}"; do
-    for PLATFORM in "${PLATFORMS[@]}"; do
+./build/run.sh make cross WHAT="${TARGETS[@]/#/cmd/}"
+
+for PLATFORM in "${PLATFORMS[@]}"; do
+    for TARGET in "${TARGETS[@]}"; do
         shasum -a 256 "_output/dockerized/bin/${PLATFORM}/${TARGET}"
         az storage blob upload \
             --account-name=oaiinfraartifacts \
             --container-name=artifacts \
             --name="kubernetes-release/release/${TAG}/bin/${PLATFORM}/${TARGET}" \
             --file="_output/dockerized/bin/${PLATFORM}/${TARGET}" \
+            --overwrite \
             --auth-mode login
+    done
+done
+
+# Reprint SHAs for convenience
+for PLATFORM in "${PLATFORMS[@]}"; do
+    for TARGET in "${TARGETS[@]}"; do
+        shasum -a 256 "_output/dockerized/bin/${PLATFORM}/${TARGET}"
     done
 done
